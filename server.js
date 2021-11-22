e921fc5eb1f7b79d733470287cdbf550aca9f40b
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-22 23:20:06 +0700

    change node version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const router = require("./src/routes");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
