9e46fdc7f4c37a0ff21a8768bd592df8c45474df
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-22 22:51:43 +0700

    deploy heroku

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/diesnatalis/invitation", router);
+app.get("/", (req, res) => {
+  res.send("Server Connected");
+});
+app.use("/invitation", router);
 
 app.listen(port, () => console.log(`Your server running on ${port}`));
