126b7567a30b0b02182271709bb8d777eba1bd5b
Rinoto Harto <rinoto.harto@phincon.com>
2021-11-22 23:25:41 +0700

    final deploy heroku

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { adminReg, login } = require("../controllers/admin");
-// const auth = require("../middlewares/auth");
 
 router.post("/register", adminReg);
 router.post("/login", login);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,29 +3,6 @@ const giftRoutes = require("./gift");
 const adminRoutes = require("./admin");
 const guestRoutes = require("./guest");
 const router = express.Router();
-// const { gifts, addGift, deleteGift, gift } = require("../controllers/gift");
-// const { guests, guest, regist, deleteGuest } = require("../controllers/guest");
-// const { adminReg, login } = require("../controllers/admin");
-// const authentication = require("../middlewares/auth");
-
-// gift
-// router.get("/gift/", gifts);
-// router.get("/gift/:id", gift);
-// router.post("/gift/", addGift);
-
-// guest
-// router.get("/guest/", guests);
-// router.get("/guest/:id", guest);
-// router.post("/guest/", regist);
-
-// admin
-// router.post("/admin/register", adminReg);
-// router.post("/admin/login", login);
-
-// Guarded Route
-// router.use(authentication);
-// router.delete("/gift/delete/:id", deleteGift);
-// router.delete("/guest/delete/:id", deleteGuest);
 
 router.use("/gift", giftRoutes);
 router.use("/admin", adminRoutes);
